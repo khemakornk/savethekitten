42709f6dc04ea597a311d02f9457436d656700b1
khemakornk <khemakornk@Khemakorns-MacBook-Pro.local>
2017-02-20 23:32:55 -0800

    Make print cute
    Hello

--- SaveTheKitten/ViewController.swift
+++ SaveTheKitten/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("Save the Kitten")
+        print("only the cute one")
     }
 
     override func didReceiveMemoryWarning() {
